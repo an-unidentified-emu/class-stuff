575a1583ae4a5d88d1058a96909ccf10e8406398
emu <an.unidentified.emu@gmail.com>
2024-11-14 13:55:47 -0500

    more stuff

--- lab3-lines.py
+++ lab3-lines.py
@@ -3,7 +3,8 @@ import numpy as np              # For working with numerical arrays
 import pandas as pd             # *NEW* For easily importing spreadsheet data
 import matplotlib.pyplot as plt # For easy plotting
 
-SLOPE_UNC = 0.0117
+SLOPE_UNC_B = 0.0117
+SLOPE_UNC_C = 0.0246
 DATE = '10/31/24'
 data = pd.read_csv("data.csv", index_col='Trial')
 # Filter rows based on TypeofForce, Angle, and Mass, then select Pull-Force column 
@@ -44,13 +45,11 @@ cStds = [
 print(bMeans)
 print(bStds)
 
+# Static Force
 static_pull_force_values = data[(data['TypeofForce'] == 'B') & (data['Angle'] == 0)]['Pull-Force']
-kinetic_pull_force_values = data[(data['TypeofForce'] == 'C') & (data['Angle'] == 0)]['Pull-Force']
-temp = data[(data['TypeofForce'] == 'B') & (data['Angle'] == 0)]['Un-Force']
-
 slope = np.sum(Gforce * bMeans) / np.sum(Gforce**2)
-slope_upper = slope + SLOPE_UNC
-slope_lower = slope - SLOPE_UNC
+slope_upper = slope + SLOPE_UNC_B
+slope_lower = slope - SLOPE_UNC_B
 
 Gforce_extended = np.linspace(0, max(Gforce) * 1.5, 100)
 
@@ -59,19 +58,40 @@ y_fit_lower = slope_lower * Gforce_extended
 y_fit_upper = slope_upper * Gforce_extended
 print(slope)
 fig1 = plt.figure()
-plt.errorbar(Gforce, bMeans, bStds, fmt='.', color='red')
-plt.plot(Gforce_extended, y_fit, color = 'green')
-# Plot thin lines for upper and lower bounds
+
+# Static force error bars and line of best fit
+plt.errorbar(Gforce, bMeans, bStds, fmt='.', color='red', label='Static Force Data')
+plt.plot(Gforce_extended, y_fit, color='red', label='Static Force Fit')
 plt.plot(Gforce_extended, y_fit_upper, color='blue', linestyle='--', linewidth=0.8)
 plt.plot(Gforce_extended, y_fit_lower, color='blue', linestyle='--', linewidth=0.8)
-plt.grid(True,'both','both')
-# Fill the area between the upper and lower bounds with a gray shade
 plt.fill_between(Gforce_extended, y_fit_lower, y_fit_upper, color='gray', alpha=0.3)
 
+# Kinetic Force
+kinetic_pull_force_values = data[(data['TypeofForce'] == 'C') & (data['Angle'] == 0)]['Pull-Force']
+slope = np.sum(Gforce * cMeans) / np.sum(Gforce**2)
+slope_upper = slope + SLOPE_UNC_C
+slope_lower = slope - SLOPE_UNC_C
+
+Gforce_extended = np.linspace(0, max(Gforce) * 1.5, 100)
+
+y_fit = slope * Gforce_extended
+y_fit_lower = slope_lower * Gforce_extended
+y_fit_upper = slope_upper * Gforce_extended
+print(slope)
+
+# Kinetic force error bars and line of best fit
+plt.errorbar(Gforce, cMeans, cStds, fmt='.', color='green', label='Kinetic Force Data')
+plt.plot(Gforce_extended, y_fit, color='green', label='Kinetic Force Fit')
+plt.plot(Gforce_extended, y_fit_upper, color='blue', linestyle='--', linewidth=0.8)
+plt.plot(Gforce_extended, y_fit_lower, color='blue', linestyle='--', linewidth=0.8)
+plt.fill_between(Gforce_extended, y_fit_lower, y_fit_upper, color='gray', alpha=0.3)
+
+# General Graph stuff
+plt.legend(loc='upper left', fontsize='small', frameon=True)
 plt.xlim(0,125)
 plt.ylim(0,45)
-
+plt.grid(True,'both','both')
 plt.xlabel("Force of Gravity (N)")
-plt.ylabel("Static Pull Force (N)")
+plt.ylabel("Pull Force (N)")
 
 plt.show()
--- lab3-static.py
+++ lab3-static.py
@@ -3,7 +3,7 @@ import numpy as np              # For working with numerical arrays
 import pandas as pd             # *NEW* For easily importing spreadsheet data
 import matplotlib.pyplot as plt # For easy plotting
 
-SLOPE_UNC = 0.0246
+SLOPE_UNC = 0.0117
 DATE = '10/31/24'
 data = pd.read_csv("data.csv", index_col='Trial')
 # Filter rows based on TypeofForce, Angle, and Mass, then select Pull-Force column 
@@ -26,25 +26,14 @@ bStds = [
     data[(data['TypeofForce'] == 'B') & (data['Angle'] == 0) & (data['Mass'] == 10556.4 ) & (data['Date'] == DATE)] ['Pull-Force'].std()
 ]
 
-cMeans = [
-    data[(data['TypeofForce'] == 'C') & (data['Angle'] == 0) & (data['Mass'] ==   556.4 ) & (data['Date'] == DATE)] ['Pull-Force'].mean(),
-    data[(data['TypeofForce'] == 'C') & (data['Angle'] == 0) & (data['Mass'] ==   956.4 ) & (data['Date'] == DATE)] ['Pull-Force'].mean(),
-    data[(data['TypeofForce'] == 'C') & (data['Angle'] == 0) & (data['Mass'] ==  5556.4 ) & (data['Date'] == DATE)] ['Pull-Force'].mean(),
-    data[(data['TypeofForce'] == 'C') & (data['Angle'] == 0) & (data['Mass'] == 10556.4 ) & (data['Date'] == DATE)] ['Pull-Force'].mean()
-]
 
-cStds = [
-    data[(data['TypeofForce'] == 'C') & (data['Angle'] == 0) & (data['Mass'] ==   556.4 ) & (data['Date'] == DATE)] ['Pull-Force'].std(),
-    data[(data['TypeofForce'] == 'C') & (data['Angle'] == 0) & (data['Mass'] ==   956.4 ) & (data['Date'] == DATE)] ['Pull-Force'].std(),
-    data[(data['TypeofForce'] == 'C') & (data['Angle'] == 0) & (data['Mass'] ==  5556.4 ) & (data['Date'] == DATE)] ['Pull-Force'].std(),
-    data[(data['TypeofForce'] == 'C') & (data['Angle'] == 0) & (data['Mass'] == 10556.4 ) & (data['Date'] == DATE)] ['Pull-Force'].std()
-]
+print(bMeans)
+print(bStds)
 
 static_pull_force_values = data[(data['TypeofForce'] == 'B') & (data['Angle'] == 0)]['Pull-Force']
-kinetic_pull_force_values = data[(data['TypeofForce'] == 'C') & (data['Angle'] == 0)]['Pull-Force']
-temp = data[(data['TypeofForce'] == 'C') & (data['Angle'] == 0)]['Un-Force']
+temp = data[(data['TypeofForce'] == 'B') & (data['Angle'] == 0)]['Un-Force']
 
-slope = np.sum(Gforce * cMeans) / np.sum(Gforce**2)
+slope = np.sum(Gforce * bMeans) / np.sum(Gforce**2)
 slope_upper = slope + SLOPE_UNC
 slope_lower = slope - SLOPE_UNC
 
@@ -55,7 +44,7 @@ y_fit_lower = slope_lower * Gforce_extended
 y_fit_upper = slope_upper * Gforce_extended
 print(slope)
 fig1 = plt.figure()
-plt.errorbar(Gforce, cMeans, cStds, fmt='.', color='red')
+plt.errorbar(Gforce, bMeans, bStds, fmt='.', color='red')
 plt.plot(Gforce_extended, y_fit, color = 'green')
 # Plot thin lines for upper and lower bounds
 plt.plot(Gforce_extended, y_fit_upper, color='blue', linestyle='--', linewidth=0.8)
